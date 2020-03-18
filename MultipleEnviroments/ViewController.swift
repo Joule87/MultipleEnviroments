00c8c80c4a2d11c3d46f545bcc94da185b911d39
Julio Collado <jcollado@altimetrik.com>
2020-03-18 17:45:24 -0300

    Base Enviroment Setup

--- MultipleEnviroments/ViewController.swift
+++ MultipleEnviroments/ViewController.swift
@@ -13,6 +13,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        if let path = Bundle.main.path(forResource: "Info", ofType: "plist"), let nsDictionaty = NSDictionary(contentsOfFile: path) {
+            let serverURL = nsDictionaty["SERVER_URL"] as? String ?? "default"
+            print("ENV SERVER: \(serverURL)")
+            let appName = nsDictionaty["CFBundleName"] as? String ?? "default"
+            print("Product Name: \(appName)")
+        }
     }
 
 
